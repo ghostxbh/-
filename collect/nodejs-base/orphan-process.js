226b850892dc3a6cc384362f6b18306d04c50212
ghostxbh <ghostxbh@hotmail.com>
2019-08-05 13:48:04 +0800

    add eval、with and orphan process

---
+++ collect/nodejs-base/orphan-process.js
@@ -0,0 +1,12 @@
+/**
+ * Created by xbh 2019-08-05 孤儿进程
+ */
+const fork = require('child_process').fork;
+const server = require('net').createServer();
+server.listen(3000);
+
+const worker = fork('./worker.js');
+
+worker.send('server', server);
+console.log('worker process created, pid: %s ppid: %s', worker.pid, process.pid);
+process.exit(0);
